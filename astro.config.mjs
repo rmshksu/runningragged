c1e2d0f374f2c1752496ea5f12c11e73e3f254c2
Rm_ssh <rmsholl@ksu.edu>
2025-09-13 10:02:08 -0500

    gonna try using vercel now instead
    new era? maybe?

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rmshksu.github.io/runningragged/",
+	site: "https://runningragged.vercel.app",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
